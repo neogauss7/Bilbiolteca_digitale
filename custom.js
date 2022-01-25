64eb79a9db5c5028a587e8129ba0521feb2f2fac
nlm23457 <96257437+nlm23457@users.noreply.github.com>
2022-01-25 18:09:47 +0100

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,19 +1,68 @@
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li')
 
-    burger.addEventListener('click', () => {
-        nav.classList.toggle('nav-active');
-    });
-    navLinks.forEach((link, index) => {
-        if(link.style.animation) {
-            link.style.animation = ''
-        } else {
-            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`;
+
+(() =>{
+ 
+  const openNavMenu = document.querySelector(".open-nav-menu"),
+  closeNavMenu = document.querySelector(".close-nav-menu"),
+  navMenu = document.querySelector(".nav-menu"),
+  menuOverlay = document.querySelector(".menu-overlay"),
+  mediaSize = 991;
+
+  openNavMenu.addEventListener("click", toggleNav);
+  closeNavMenu.addEventListener("click", toggleNav);
+ 
+  menuOverlay.addEventListener("click", toggleNav);
+
+  function toggleNav() {
+  	navMenu.classList.toggle("open");
+  	menuOverlay.classList.toggle("active");
+  	document.body.classList.toggle("hidden-scrolling");
+  }
+
+  navMenu.addEventListener("click", (event) =>{
+      if(event.target.hasAttribute("data-toggle") && 
+      	window.innerWidth <= mediaSize){
+      	
+      	event.preventDefault();
+      	const menuItemHasChildren = event.target.parentElement;
+        
+        if(menuItemHasChildren.classList.contains("active")){
+        	collapseSubMenu();
         }
-    });
+        else{
+          
+          if(navMenu.querySelector(".menu-item-has-children.active")){
+        	collapseSubMenu();
+          }
+          
+          menuItemHasChildren.classList.add("active");
+          const subMenu = menuItemHasChildren.querySelector(".sub-menu");
+          subMenu.style.maxHeight = subMenu.scrollHeight + "px";
+        }
+      }
+  });
+  function collapseSubMenu(){
+  	navMenu.querySelector(".menu-item-has-children.active .sub-menu")
+  	.removeAttribute("style");
+  	navMenu.querySelector(".menu-item-has-children.active")
+  	.classList.remove("active");
+  }
+  function resizeFix(){
+  	 
+  	 if(navMenu.classList.contains("open")){
+  	 	toggleNav();
+  	 }
+  	 
+  	 if(navMenu.querySelector(".menu-item-has-children.active")){
+        	collapseSubMenu();
+     }
+  }
 
-}
+  window.addEventListener("resize", function(){
+     if(this.innerWidth > mediaSize){
+     	resizeFix();
+     }
+  });
 
-navSlide();
+})();
+
