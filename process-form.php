20bdb314652e116c85252c10735ce22c306d6088
luigimiraglia <96987926+luigimiraglia@users.noreply.github.com>
2022-05-27 16:48:23 +0200

    Update process-form.php

--- process-form.php
+++ process-form.php
@@ -1,1 +1,16 @@
+<?php
 
+$name = $_POST['name'];
+
+$conn = new mysqli('localhost', 'root', '', 'test');
+if($conn->connect_error){
+    die('Connection Failed : '.$conn->connect_error);
+
+}else{
+    $stmt = $conn->prepare("insert into registration(name) values(?)");
+    $stmt->bind_param("s", $name);
+    echo "reg succ";
+    $stmt->close();
+    $conn->close();
+}
+?>
