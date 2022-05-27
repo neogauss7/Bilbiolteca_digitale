704bcb3c56701cacc95e2ea3c0e3dd78560d9172
luigimiraglia <96987926+luigimiraglia@users.noreply.github.com>
2022-05-27 19:01:45 +0200

    Update process-form.php

--- process-form.php
+++ process-form.php
@@ -1,16 +1,28 @@
+
 <?php
 
-$name = $_POST['name'];
+include_once("/code/configs.php");
 
-$conn = new mysqli('localhost', 'root', '', 'test');
-if($conn->connect_error){
-    die('Connection Failed : '.$conn->connect_error);
+$servername = "mysql.theoremz.com";
+$username = "theoremzcom1";
+$password = "Bpgeczvx";
+$dbname = "mysql.theoremz.com";
 
-}else{
-    $stmt = $conn->prepare("insert into registration(name) values(?)");
-    $stmt->bind_param("s", $name);
-    echo "reg succ";
-    $stmt->close();
-    $conn->close();
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
 }
+
+$sql = "INSERT INTO MyGuests (name)
+VALUES ('John', 'Doe', 'john@example.com')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
 ?>
