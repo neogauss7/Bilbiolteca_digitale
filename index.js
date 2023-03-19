f9bf9d650167ef2ab79585b5329cc7da9bfb7b5d
luigimiraglia <96987926+luigimiraglia@users.noreply.github.com>
2023-03-19 10:27:54 +0100

    j

--- index.js
+++ index.js
@@ -95,7 +95,6 @@ function login() {
       database_ref.child("users/" + user.uid).update(user_data);
 
       // DOne
-      alert("User Logged In!!");
     })
     .then(() => {
       location.href = "index.html";
